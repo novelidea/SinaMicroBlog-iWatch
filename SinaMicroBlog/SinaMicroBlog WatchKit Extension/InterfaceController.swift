dbe6237992b3802e3f6772fad43b2aab1b0a9027
XingPengfei <novelidea@XingPengfeis-MacBook-Pro.local>
2015-12-28 11:22:17 -0800

    init

---
+++ SinaMicroBlog/SinaMicroBlog WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SinaMicroBlog WatchKit Extension
+//
+//  Created by XingPengfei on 12/28/15.
+//  Copyright © 2015 Pengfei Xing. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ SinaMicroBlog/SinaMicroBlog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinaMicroBlog
+//
+//  Created by XingPengfei on 12/28/15.
+//  Copyright © 2015 Pengfei Xing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
